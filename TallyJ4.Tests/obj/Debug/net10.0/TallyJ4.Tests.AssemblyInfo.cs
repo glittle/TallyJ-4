3d355c3cd5c20e5a547761a2bf85399c1ce41d91
Glen Little <glen.little@gmail.com>
2026-01-24 15:07:18 -0700

    remove bin and obj from git

--- TallyJ4.Tests/obj/Debug/net10.0/TallyJ4.Tests.AssemblyInfo.cs
+++ TallyJ4.Tests/obj/Debug/net10.0/TallyJ4.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TallyJ4.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b542b4d87c217b1b7350073cda6cd6d13245597c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc9aad4deb839786b481283dd40de31390c4fb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TallyJ4.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TallyJ4.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
