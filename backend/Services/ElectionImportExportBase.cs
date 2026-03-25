4873e3fc02c4dfab924793fc7478d0f4f09ec4ce
Glen Little <glen.little@gmail.com>
2026-03-25 00:02:13 -0600

    Remove unused imports from ElectionImportExportBase and add FUNDING.yml file

--- backend/Services/ElectionImportExportBase.cs
+++ backend/Services/ElectionImportExportBase.cs
@@ -1,12 +1,7 @@
 using Backend.Domain.Entities;
-using Backend.Domain.Enumerations;
 using Backend.Domain.Context;
-using Backend.DTOs.Import;
-using Backend.DTOs.Elections;
 using Backend.Models;
-using Microsoft.EntityFrameworkCore;
 using System.Globalization;
-using System.Text.Json;
 using System.Xml;
 using System.Xml.Schema;
 
