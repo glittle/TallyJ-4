d72e18f9a7d0b13acb6ecbfcc5ca169aa81a7032
Glen Little <glen.little@gmail.com>
2026-04-04 11:57:11 -0600

    switch style to LESS; improve input boxes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import { createApp } from "vue";
 
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-import "./style.css";
+import "./style.less";
 
 import App from "./App.vue";
 import { loadAppConfig, setAppConfig } from "./config/appConfig";
